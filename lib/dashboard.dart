22ab56b4bb99fa00fb985dc7a7179f41fd0494a0
yanditv <yankur44@gmail.com>
2024-05-02 20:42:48 -0500

    updated

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,8 +5,73 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(child: Text("Dashboard"))),
+    var ancho = MediaQuery.of(context).size.width;
+    var alto = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "Mi perfil",
+          style: TextStyle(color: Colors.grey, fontSize: 18),
+        )),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.notifications,
+              color: Colors.grey[400],
+              size: 25,
+            ),
+          )
+        ],
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[400],
+          size: 35.0,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            width: ancho,
+            height: 200,
+            child: Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 250,
+                  color: Colors.amber,
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Junior Wachapa",
+                      textAlign: TextAlign.left,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.card_giftcard),
+                        Text("090931231231231"),
+                        Text("12.50"),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
